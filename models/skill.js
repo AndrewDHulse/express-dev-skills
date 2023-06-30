62f22ce4e7f8b9f90365c0795ecd336e79769a3b
AndrewDHulse <theandrewhulse@gmail.com>
2023-06-29 20:22:35 -0500

    part 2 of homework complete

--- models/skill.js
+++ models/skill.js
@@ -4,6 +4,23 @@ const skills = [
     {id:3, skill: 'JavaScript'}
 ];
 
+function update(id, updatedSkill){
+    id=parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill)
+}
+
+function deleteOne(id){
+    id=parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill){
+    skill.id = Date.now() % 1000000;
+    skills.push(skill);
+}
+
 function getAll(){
     return skills;
 };
@@ -15,5 +32,8 @@ function getOne(id){
 
 module.exports={
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
 };
--- routes/skills.js
+++ routes/skills.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
 const skillsCtrl = require('../controllers/skills')
 
 /* GET /skills */
 router.get('/', skillsCtrl.index);
-
+//get skills/new
+router.get('/new', skillsCtrl.new);
 //GETs /skills/:id
 router.get('/:id', skillsCtrl.show);
+//get /skills/:id/edit
+router.get('/:id/edit', skillsCtrl.edit);
+//post /skills
+router.post('/', skillsCtrl.create);
+//delete /skills/:id
+router.delete('/:id', skillsCtrl.delete);
+//put /skills/:id
+router.put('/:id', skillsCtrl.update);
 
 module.exports = router;
